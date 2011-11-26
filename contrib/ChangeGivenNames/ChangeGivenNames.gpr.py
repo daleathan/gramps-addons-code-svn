9fc565a0aa22d41b4607ab6bab26759bc6584101
dsblank <dsblank@1b16344b-c707-4684-8e57-f14b83debe0b>
2011-11-26 18:49:27 +0000

    Added glade file

--- contrib/ChangeGivenNames/ChangeGivenNames.gpr.py
+++ contrib/ChangeGivenNames/ChangeGivenNames.gpr.py
@@ -36,7 +36,7 @@ id    = 'chgivenname',
 name  = _("Fix Capitalization of Given Names"),
 description =  _("Searches the entire database and attempts to "
                     "fix capitalization of the given names."),
-version = '1.0.1',
+version = '1.0.2',
 gramps_target_version = '3.3',
 status = STABLE,
 fname = 'ChangeGivenNames.py',
