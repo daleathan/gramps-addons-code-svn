6040dd881911a7666f57ddac1247089c47949141
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-08-19 05:56:12 +0000

    5974: remove Data Entry gramplet from listing files via "include_in_listing" argument into gpr.py

--- contrib/DataEntryGramplet/DataEntryGramplet.gpr.py
+++ contrib/DataEntryGramplet/DataEntryGramplet.gpr.py
@@ -13,11 +13,12 @@ register(GRAMPLET,
          gramplet_title=_("Data Entry"),
          detached_width = 510,
          detached_height = 480,
-         version = '1.0.17',
+         version = '1.0.18',
          gramps_target_version = '3.4',
          status=UNSTABLE,
          fname="DataEntryGramplet.py",
          help_url="Data Entry Gramplet",
+         include_in_listing = False,
          )
 
 
