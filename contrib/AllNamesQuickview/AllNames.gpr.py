528ca4cf5fafefd8e4683d81e4a7c49ebedc91a3
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2013-02-25 08:59:38 +0000

    6297: update status after tests by Peter (python3, reviews)

--- contrib/AllNamesQuickview/AllNames.gpr.py
+++ contrib/AllNamesQuickview/AllNames.gpr.py
@@ -4,7 +4,7 @@ register(QUICKREPORT,
          description= _("Display all names of all people"),
          version = '1.0.11',
          gramps_target_version = '4.0',
-         status = STABLE, # not yet tested with python 3
+         status = UNSTABLE, # TypeError: unorderable types: Person() < Person()
          fname = 'AllNames.py',
          authors = ["Douglas Blank"],
          authors_email = ["dblank@cs.brynmawr.edu"],
