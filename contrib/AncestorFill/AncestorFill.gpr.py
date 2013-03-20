99cb8ce03fae26331e4c1144e5a1499bc2a9ddd7
ericdout <ericdout@1b16344b-c707-4684-8e57-f14b83debe0b>
2013-03-20 19:59:20 +0000

    Report on the filling of the tree

---
+++ contrib/AncestorFill/AncestorFill.gpr.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------
+#
+# register_report
+#
+#-------------------------------------------------------------------------
+register(REPORT,
+  id    = 'AncestorFill',
+  name  = _("AncestorFill"),
+  description =  _("Report on the filling of the tree"),
+  version = '1.0',
+  gramps_target_version = '3.4',
+  status = STABLE,
+  fname = 'AncestorFill.py',
+  authors = ["Eric Doutreleau"],
+  authors_email = ["eric@doutreleau.fr"],
+  category = CATEGORY_TEXT,
+  require_active = False,
+  reportclass = 'AncestorFillReport',
+  optionclass = 'AncestorFillOptions',
+  report_modes = [REPORT_MODE_GUI, REPORT_MODE_CLI],
+)
