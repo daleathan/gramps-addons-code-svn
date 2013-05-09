88e6a858cf897dff888a646e8d2b5470e6f06034
nick-h <nick-h@1b16344b-c707-4684-8e57-f14b83debe0b>
2013-05-09 23:17:26 +0000

    Flag DescendantCount and NumberOfDescendantsQuickview as STABLE

--- contrib/NumberOfDescendantsQuickview/NumberOfDescendantsQuickview.gpr.py
+++ contrib/NumberOfDescendantsQuickview/NumberOfDescendantsQuickview.gpr.py
@@ -4,7 +4,7 @@ register(QUICKREPORT,
          description= _("Shows the number of descendants of the current person"),
          version = '3.4.15',
          gramps_target_version = '4.0',
-         status = UNSTABLE, # not yet tested with python 3, error on unicode row
+         status = STABLE,
          fname = 'NumberOfDescendantsQuickview.py',
          authors = ["Reinhard Mueller"],
          authors_email = ["reinhard.mueller@igal.at"],
