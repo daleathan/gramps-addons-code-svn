54b2a402097183f09059b8c09f2d7f5274f39dfb
dsblank <dsblank@1b16344b-c707-4684-8e57-f14b83debe0b>
2013-12-01 19:21:37 +0000

    Wordle Gramplet, for use for wordle.net

---
+++ contrib/WordleGramplet/WordleGramplet.gpr.py
@@ -0,0 +1,16 @@
+register(
+    GRAMPLET, 
+    id= "Wordle Gramplet", 
+    name=_("Wordle Gramplet"), 
+    status = STABLE,
+    fname="WordleGramplet.py",
+    height=230,
+    gramplet = 'WordleGramplet',
+    gramplet_title=_("Wordle"),
+    gramps_target_version = '3.4',
+    version = '1.0.7',
+    description = "Gramplet used to make word clouds with wordle.net",
+    authors = ["Douglas Blank"],
+    authors_email = ["doug.blank@gmail.com"],
+    )
+
