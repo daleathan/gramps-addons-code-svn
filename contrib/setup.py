29e1a1259ee51f0fbc22937a0fdc3a836accbf53
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2014-10-12 10:12:06 +0000

    7395: step7 with polish

--- contrib/setup.py
+++ contrib/setup.py
@@ -747,7 +747,8 @@ def listing():
                 gpr = open(gpr_file.encode("utf-8", errors="backslashreplace"))
  
                 plug = dict([file.strip(), None] for file in gpr if file.strip())
-                print(plug['status = STABLE,'])
+                for p in plug:
+                    print(repr(p))
 
                 #code = compile(gpr.read(),
                                    #gpr_file.encode("utf-8", errors="backslashreplace"),
