cb91bee11af0c5a4cd17afefd15d177dc7b67e9b
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2014-10-27 19:49:37 +0000

    fix register() on SourcesCitationsReport

--- contrib/SourcesCitationsReport/SourcesCitationsReport.gpr.py
+++ contrib/SourcesCitationsReport/SourcesCitationsReport.gpr.py
@@ -5,19 +5,18 @@
 #------------------------------------------------------------------------
 
 register(REPORT,
-         id    = 'SourcesCitationsReport'
-         name  = _("Sources and Citations Report")
-         description =  _("Provides a source and Citations with notes")
-         version = '1.0'
-         gramps_target_version = '4.1'
-         status = STABLE
-         fname = 'SourcesCitationsReport.py'
-         ptype = REPORT
-         authors = ["Uli22"]
-         authors_email = ["hansulrich.frink@gmail.com"]
-         category = CATEGORY_TEXT
-         reportclass = 'SourcesCitationsReport'
-         optionclass = 'SourcesCitationsOptions'
-         report_modes = [REPORT_MODE_GUI, REPORT_MODE_BKI, REPORT_MODE_CLI]
-         require_active = False
+         id    = 'SourcesCitationsReport',
+         name  = _("Sources and Citations Report"),
+         description =  _("Provides a source and Citations with notes"),
+         version = '1.0',
+         gramps_target_version = '4.1',
+         status = STABLE,
+         fname = 'SourcesCitationsReport.py',
+         authors = ["Uli22"],
+         authors_email = ["hansulrich.frink@gmail.com"],
+         category = CATEGORY_TEXT,
+         reportclass = 'SourcesCitationsReport',
+         optionclass = 'SourcesCitationsOptions',
+         require_active = False,
+         report_modes = [REPORT_MODE_GUI, REPORT_MODE_BKI, REPORT_MODE_CLI],
          )
