0be39f7d93edf1c68261fa783b8f77e7a893a32f
grocanar <grocanar@1b16344b-c707-4684-8e57-f14b83debe0b>
2015-03-22 22:04:56 +0000

    update AncestorFill

--- contrib/AncestorFill/AncestorFill.py
+++ contrib/AncestorFill/AncestorFill.py
@@ -37,97 +37,18 @@ import gettext
 # gramps modules
 #
 #------------------------------------------------------------------------
-import const
-from gen.display.name import displayer as global_name_display
-from Errors import ReportError
-from gen.lib import ChildRefType
-from gen.plug.menu import (NumberOption, PersonOption,BooleanOption,
-                          EnumeratedListOption)
-from gen.plug.docgen import (IndexMark, FontStyle, ParagraphStyle,
-                             FONT_SANS_SERIF, INDEX_TYPE_TOC, 
-                             PARA_ALIGN_CENTER)
-from gen.plug.report import Report
-from gen.plug.report import MenuReportOptions
-from libtranslate import get_language_string
-from TransUtils import get_addon_translator
-from libtranslate import Translator, get_language_string
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+_ = glocale.get_addon_translator(__file__).gettext
+from gramps.gen.display.name import displayer as global_name_display
+from gramps.gen.errors import ReportError
+from gramps.gen.lib import ChildRefType
+from gramps.gen.plug.menu import (NumberOption, PersonOption, BooleanOption, EnumeratedListOption)
+from gramps.gen.plug.docgen import (IndexMark, FontStyle, ParagraphStyle, FONT_SANS_SERIF, INDEX_TYPE_TOC, PARA_ALIGN_CENTER)
+from gramps.gen.plug.report import Report
+from gramps.gen.plug.report import MenuReportOptions
+from gramps.gen.plug.report import stdoptions
 from collections import defaultdict
 import logging
-
-_ = get_addon_translator().ugettext
-
-LOCALEDIR = os.path.join(const.USER_PLUGINS, 'AncestorFill', 'locale')
-LOCALEDOMAIN = 'addon'
-
-# see TransUtils.py
-
-def get_available_translations():
-    """
-    Get a list of available translations.
-
-    :returns: A list of translation languages.
-    :rtype: unicode[]
-    
-    """
-    languages = []
-
-    if LOCALEDIR is None:
-        return languages
-
-    for langdir in os.listdir(LOCALEDIR):
-        mofilename = os.path.join( LOCALEDIR, langdir,
-                                   "LC_MESSAGES", "addon.mo")
-        _LOG = logging.getLogger()
-        msg = "MOD file " + mofilename
-        _LOG.debug("message %s\n", msg)
-        if os.path.exists(mofilename):
-            _LOG = logging.getLogger()
-            msg = "LANG file " + langdir
-            _LOG.debug("message %s\n", msg)
-            languages.append(langdir)
-
-    languages.sort()
-
-    return languages
-
-class Translator:
-    """
-    This class provides translated strings for the configured language.
-    """
-
-    def __init__(self, lang="en"):
-        """
-        :param lang: The language to translate to. 
-            The language can be:
-               * The name of any installed .mo file
-               * "en" to use the message strings in the code
-               * "default" to use the default translation being used by gettext.
-        :type lang: string
-        :return: nothing
-        
-        """
-        if lang == "en":
-            self.__trans = None
-        else:
-            # fallback=True will cause the translator to use English if 
-            # lang = "en" or if something goes wrong.
-            self.__trans = gettext.translation(LOCALEDOMAIN, LOCALEDIR,
-                                       [lang], fallback = True)
-
-    def gettext(self, message):
-        """
-        Return the unicode translated string.
-        
-        :param message: The message to be translated.
-        :type message: string
-        :returns: The translated message
-        :rtype: unicode
-        
-        """
-        if self.__trans:
-            return self.__trans.gettext(message)
-        else:
-            return unicode(gettext.gettext(message))
 
 #------------------------------------------------------------------------
 #
@@ -141,7 +62,7 @@ class AncestorFillReport(Report):
     def __init__(self, database, options, user):
         """
         Create the AncestorFillReport object that produces the AncestorFill report.
-        
+
         The arguments are:
 
         database        - the GRAMPS database instance
@@ -160,7 +81,7 @@ class AncestorFillReport(Report):
 
         self.trouve = {}
         self.index = defaultdict(list)
-        self.gener = defaultdict(lambda : defaultdict(int))
+        self.gener = defaultdict(lambda: defaultdict(int))
         menu = options.menu
         self.max_generations = menu.get_option_by_name('maxgen').get_value()
         pid = menu.get_option_by_name('pid').get_value()
@@ -168,16 +89,15 @@ class AncestorFillReport(Report):
         self.Collapsedigit = menu.get_option_by_name('Collapsedigit').get_value()
         self.displayth = menu.get_option_by_name('Display theorical').get_value()
         self.center_person = database.get_person_from_gramps_id(pid)
-        if (self.center_person == None) :
-            raise ReportError(_("Person %s is not in the Database") % pid )
-        language = menu.get_option_by_name('trans').get_value()
-        translator = Translator(language)
+        if self.center_person == None:
+            raise ReportError(_("Person %s is not in the Database") % pid)
 
-        # Copy the global NameDisplay so that we don't change application 
+        # Copy the global NameDisplay so that we don't change application
         # defaults.
         self._name_display = copy.deepcopy(global_name_display)
+        lang = options.menu.get_option_by_name('trans').get_value()
+        self._locale = self.set_locale(lang)
 
-        self._ = translator.gettext
 
     def apply_filter(self, person_handle, index, generation=1):
         """
@@ -198,12 +118,12 @@ class AncestorFillReport(Report):
 
         person = self.database.get_person_from_handle(person_handle)
         grampsid = person.get_gramps_id()
-	
-        if self.trouve.has_key(grampsid):
-            return
-        
 
-        # store the person in the map based off their index number 
+        if grampsid in self.trouve:
+            return
+
+
+        # store the person in the map based off their index number
         # which is passed to the routine.
         father_handle = None
         mother_handle = None
@@ -214,13 +134,13 @@ class AncestorFillReport(Report):
             # the passed person. There should be exactly one, but there is
             # nothing that prevents the same child in the list multiple times.
 
-            ref = [ c for c in family.get_child_ref_list()
+            ref = [c for c in family.get_child_ref_list()
                     if c.get_reference_handle() == person_handle]
             if ref:
 
                 # If the father_handle is not defined and the relationship is
-                # BIRTH, then we have found the birth father. Same applies to 
-                # the birth mother. If for some reason, the we have multiple 
+                # BIRTH, then we have found the birth father. Same applies to
+                # the birth mother. If for some reason, the we have multiple
                 # people defined as the birth parents, we will select based on
                 # priority in the list
 
@@ -231,7 +151,7 @@ class AncestorFillReport(Report):
                    ref[0].get_mother_relation() == ChildRefType.BIRTH:
                     mother_handle = family.get_mother_handle()
 
-        # Recursively call the function. It is okay if the handle is None,  
+        # Recursively call the function. It is okay if the handle is None,
         # since routine handles a handle of None
 
         fatherid = False
@@ -262,6 +182,7 @@ class AncestorFillReport(Report):
         """
 
         name = self._name_display.display(self.center_person)
+        gettext = self._locale.translation.gettext
         self.title = _("AncestorFill for %s") % name
         self.doc.start_paragraph("AHN-Title")
         mark = IndexMark(self.title, INDEX_TYPE_TOC, 1)
@@ -282,25 +203,25 @@ class AncestorFillReport(Report):
         strtheoanc = _(" Number of theorical Ancestors ")
         strimplex = _(" Pedigree Collapse ")
         if self.displayth:
-	        form = strgen + "%2d; " + strfoundanc + "%12d;" + strtheoanc + str(theor) + "; " + pctfoundanc +" %." + str(self.Filleddigit) + "f%% " + " ; " + uniqfoundanc + " %6d; " + strimplex + "%3." + str(self.Collapsedigit) + "f%%"
+            form = strgen + "%2d; " + strfoundanc + "%12d;" + strtheoanc + str(theor) + "; " + pctfoundanc +" %." + str(self.Filleddigit) + "f%% " + " ; " + uniqfoundanc + " %6d; " + strimplex + "%3." + str(self.Collapsedigit) + "f%%"
         else:
             form = strgen + "%2d; " + strfoundanc + "%12d;" + pctfoundanc +" %." + str(self.Filleddigit) + "f%% " + " ; " + uniqfoundanc + " %6d; " + strimplex + "%3." + str(self.Collapsedigit) + "f%%"
-            text = _( 
-                       form % ( gen , longueur , percent , nbhand , implexe ))
+            text = gettext(
+                       form % ( gen, longueur, percent, nbhand, implexe ))
             self.doc.start_paragraph("AHN-Generation")
             self.doc.write_text(text)
             self.doc.end_paragraph()
         for gen in range(0, self.max_generations):
             nextgen = gen + 1
-            for gid in self.gener[gen].keys():
+            for gid in list(self.gener[gen].keys()):
                 for id2 in self.index[gid]:
                     if id2:
                         self.gener[nextgen][id2] += self.gener[gen][gid]
             longueur = 0
             nbhand = 0
-            msg = "GEN  " + str (nextgen)
-            for hand in self.gener[nextgen].keys():
-                msg = msg + " " + str(hand) 
+            msg = "GEN  " + str(nextgen)
+            for hand in list(self.gener[nextgen].keys()):
+                msg = msg + " " + str(hand)
                 longueur += self.gener[nextgen][hand]
                 nbhand += 1
             theor = 2 ** nextgen
@@ -315,21 +236,24 @@ class AncestorFillReport(Report):
             else:
                 self.doc.start_paragraph("AHN-Generation")
                 if self.displayth:
-		    form = strgen + "%2d; " + strfoundanc + "%12d;" + strtheoanc + str(theor) + "; " + pctfoundanc +" %." + str(self.Filleddigit) + "f%% " + " ; " + uniqfoundanc + " %6d; " + strimplex + "%3." + str(self.Collapsedigit) + "f%%"
+                    form = strgen + "%2d; " + strfoundanc + "%12d;" + strtheoanc + str(theor) + "; " + pctfoundanc +" %." + str(self.Filleddigit) + "f%% " + " ; " + uniqfoundanc + " %6d; " + strimplex + "%3." + str(self.Collapsedigit) + "f%%"
                 else:
-		    form = strgen + "%2d; " + strfoundanc + "%12d;" + pctfoundanc +" %." + str(self.Filleddigit) + "f%% " + " ; " + uniqfoundanc + " %6d; " + strimplex + "%3." + str(self.Collapsedigit) + "f%%"
-                text = _( 
-                       form % ( nextgen , longueur , percent , nbhand , implexe ))
+                    form = strgen + "%2d; " + strfoundanc + "%12d;" + pctfoundanc +" %." + str(self.Filleddigit) + "f%% " + " ; " + uniqfoundanc + " %6d; " + strimplex + "%3." + str(self.Collapsedigit) + "f%%"
+                text = gettext(
+                       form % (nextgen, longueur, percent, nbhand, implexe))
                 self.doc.write_text(text)
                 self.doc.end_paragraph()
         totalnbanc = len(self.trouve)
-        timplexe= ( total - totalnbanc) * 100.0 / total
+        if total:
+            timplexe= (total - totalnbanc) * 100.0 / total
+        else:
+            timplexe = 0
         strtotalanc = _(" Total Number of Ancestors found ")
-	form = strtotalanc + "%d; "
+        form = strtotalanc + "%d; "
         totaluniqfoundanc = _(" Total Number of single Ancestors found ")
         form2 = totaluniqfoundanc + "%d; "
         form3 = strimplex + "%3." + str(self.Collapsedigit) + "f%%"
-        text = _( form % ( total )) + " " + _( form2 % ( totalnbanc )) + _( form3 % ( timplexe ))
+        text = gettext(form %(total)) + " " + _(form2 %(totalnbanc)) + _(form3 % (timplexe))
         self.doc.start_paragraph("AHN-Generation")
         self.doc.write_text(text)
         self.doc.end_paragraph()
@@ -349,34 +273,29 @@ class AncestorFillOptions(MenuReportOptions):
 
     def __init__(self, name, dbase):
         MenuReportOptions.__init__(self, name, dbase)
-        
+
     def add_menu_options(self, menu):
         """
         Add options to the menu for the ancestor report.
         """
-        category_name = _("Report Options")
-        
+        category = _("Report Options")
+
         pid = PersonOption(_("Center Person"))
         pid.set_help(_("The center person for the report"))
-        menu.add_option(category_name, "pid", pid)
+        menu.add_option(category, "pid", pid)
         maxgen = NumberOption(_("Generations"), 10, 1, 300)
         maxgen.set_help(_("The number of generations to include in the report"))
-        menu.add_option(category_name, "maxgen", maxgen)
+        menu.add_option(category, "maxgen", maxgen)
         Filleddigit = NumberOption(_("Filleddigit"), 10, 1, 50)
         Filleddigit.set_help(_("The number of digit after comma to include in the report for the percentage of ancestor found at a given generation"))
-        menu.add_option(category_name, "Filleddigit", Filleddigit)
+        menu.add_option(category, "Filleddigit", Filleddigit)
         Collapsedigit = NumberOption(_("Collapsedigit"), 10, 1, 50)
         Collapsedigit.set_help(_("The number of digit after comma to include in the report for the pedigree Collapse"))
-        menu.add_option(category_name, "Collapsedigit", Collapsedigit)
+        menu.add_option(category, "Collapsedigit", Collapsedigit)
         displayth = BooleanOption(_("Display theorical"), False)
         displayth.set_help(_("Display the theorical number of ancestor by generation"))
-        menu.add_option(category_name, "Display theorical", displayth)
-        trans = EnumeratedListOption(_("Translation"),"default") 
-        trans.add_item("default", _("English"))
-        for language in get_available_translations():
-            trans.add_item(language, get_language_string(language))
-        trans.set_help(_("The translation to be used for the report."))
-        menu.add_option(category_name, "trans", trans)
+        menu.add_option(category, "Display theorical", displayth)
+        stdoptions.add_localization_option(menu, category)
 
     def make_default_style(self, default_style):
         """
@@ -400,7 +319,7 @@ class AncestorFillOptions(MenuReportOptions):
                         14pt
             Paragraph : Second level header
                         0.125cm top and bottom margins
-                        
+
         AHN - Normal text display for each entry
 
             Font      : default
@@ -418,10 +337,10 @@ class AncestorFillOptions(MenuReportOptions):
         para.set_header_level(1)
         para.set_top_margin(0.25)
         para.set_bottom_margin(0.25)
-        para.set_alignment(PARA_ALIGN_CENTER)       
+        para.set_alignment(PARA_ALIGN_CENTER)
         para.set_description(_('The style used for the title of the page.'))
         default_style.add_paragraph_style("AHN-Title", para)
-    
+
         #
         # AHN-Generation
         #
@@ -434,13 +353,13 @@ class AncestorFillOptions(MenuReportOptions):
         para.set_bottom_margin(0.125)        
         para.set_description(_('The style used for the generation header.'))
         default_style.add_paragraph_style("AHN-Generation", para)
-    
+
         #
         # AHN-Entry
         #
         para = ParagraphStyle()
         para.set(first_indent=-1.0, lmargin=1.0)
         para.set_top_margin(0.125)
-        para.set_bottom_margin(0.125)        
+        para.set_bottom_margin(0.125)
         para.set_description(_('The basic style used for the text display.'))
         default_style.add_paragraph_style("AHN-Entry", para)
